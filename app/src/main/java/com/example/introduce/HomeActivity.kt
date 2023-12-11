92492a2be27c1d973cfa7755eb7e4450df67b693
ho lee <dlho1222@naver.com>
2023-12-11 14:24:15 +0900

    2. 자기 소개 랜덤사진

--- app/src/main/java/com/example/introduce/HomeActivity.kt
+++ app/src/main/java/com/example/introduce/HomeActivity.kt
@@ -2,18 +2,29 @@ package com.example.introduce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import org.w3c.dom.Text
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
+    val images = listOf(R.drawable.android_original,R.drawable.android_pressed2,R.drawable.android_pressed,R.drawable.cpu,R.drawable.smile_icon)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val profile = findViewById<ImageView>(R.id.iv_Profile)
+        val finishButton = findViewById<Button>(R.id.btn_finish)
         val id = intent.getStringExtra("id")
         val tv_Id = findViewById<TextView>(R.id.tv_Id)
         val tv_name = findViewById<TextView>(R.id.tv_Name)
         val tv_age = findViewById<TextView>(R.id.tv_Age)
         val tv_MBTI = findViewById<TextView>(R.id.tv_MBTI)
+        profile.setImageResource(images[Random.nextInt(4)])
+        finishButton.setOnClickListener {
+            finish()
+        }
+
         tv_Id.text = "아이디: $id"
         tv_name.text = "이름: 이호"
         tv_age.text = "나이: 29"
