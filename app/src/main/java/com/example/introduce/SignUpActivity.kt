90ceff3e5369f499113c1285621da2e4a97cc759
ho lee <dlho1222@naver.com>
2023-12-15 19:53:40 +0900

    비밀번호 정규식 이용 유효성 검사, 값 저장

--- app/src/main/java/com/example/introduce/SignUpActivity.kt
+++ app/src/main/java/com/example/introduce/SignUpActivity.kt
@@ -1,5 +1,6 @@
 package com.example.introduce
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +16,7 @@ import android.widget.ListAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.edit
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import java.util.regex.Pattern
@@ -42,26 +44,73 @@ class SignUpActivity : AppCompatActivity() {
         focusOut(confirmPassword)
 
         signUpButton.setOnClickListener {
-            if (id.text.isNullOrEmpty() || confirmPassword.text.isNullOrEmpty() || password.text.isNullOrEmpty() || email.text.isNullOrEmpty()) {
+            if (id.text.isNullOrEmpty() || confirmPassword.text.isNullOrEmpty() || password.text.isNullOrEmpty() || email.text.isNullOrEmpty() || emailForm.text.isNullOrEmpty()) {
                 Toast.makeText(this, "입력값이 비어있습니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
-            } else {
-                if(isEqualPassword(confirmPassword)){
+            } else {//모든 필드가 입력된 상태
+                if (isEqualPassword() && check(password)) { //유효성 검사 후 승인
                     Intent().apply {
                         putExtra("updatedId", id.text.toString())
                         putExtra("updatedPassword", password.text.toString())
                         setResult(RESULT_OK, this)
                     }
+                    saveData()
                     finish()
+                } else if (check(password)) { //password 와 confirmPassword가 같을 때 처리
+                    Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                } else { //둘 다 아닐 때
+                    Toast.makeText(this, "비밀번호는 10자리 이상, 특수문자, 숫자포함 입니다.", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
             }
         }
 
+    }
+
+    private fun saveData() {   //입력 값 저장
+        val id = findViewById<EditText>(R.id.et_Id)
+        val password = findViewById<EditText>(R.id.et_Password)
+        val confirmPassword = findViewById<EditText>(R.id.et_ConfirmPassword)
+        val email = findViewById<EditText>(R.id.et_Email)
+        val emailForm = findViewById<EditText>(R.id.et_EmailForm)
+        getSharedPreferences(INFO, Context.MODE_PRIVATE)?.edit {
+            putString(ID, id.text.toString())
+            putString(EMAIL, email.text.toString())
+            putString(EMAIL_FORM, emailForm.text.toString())
+            putString(PASSWORD, password.text.toString())
+            putString(CONFIRM_PASSWORD, confirmPassword.text.toString())
+            apply()
+        }
+    }
+
+    private fun getSaveData() {  //저장한 값 불러오기
+        val id = findViewById<EditText>(R.id.et_Id)
+        val password = findViewById<EditText>(R.id.et_Password)
+        val confirmPassword = findViewById<EditText>(R.id.et_ConfirmPassword)
+        val email = findViewById<EditText>(R.id.et_Email)
+        val emailForm = findViewById<EditText>(R.id.et_EmailForm)
+        getSharedPreferences(INFO, Context.MODE_PRIVATE).run {
+            id.setText(getString(ID, ""))
+            email.setText(getString(EMAIL, ""))
+            password.setText(getString(PASSWORD, ""))
+            confirmPassword.setText(getString(CONFIRM_PASSWORD, ""))
+            emailForm.setText(getString(EMAIL_FORM, ""))
+        }
 
     }
 
-    private fun focusOut(editText: EditText) {
+    override fun onResume() {  //onResume 시 값 불러옴
+        getSaveData()
+        super.onResume()
+    }
+
+    override fun onDestroy() {  //onDestroy 시 값 저장
+        saveData()
+        super.onDestroy()
+    }
+
+    private fun focusOut(editText: EditText) {//EditText가 비어 있을때 포커스 아웃처리
         editText.setOnFocusChangeListener { v, hasFocus ->
             if (hasFocus) {
             } else {
@@ -79,36 +128,47 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun validation(editText: EditText) {
+    private fun validation(editText: EditText) {  //각 필드 유효성 체크
         editText.addTextChangedListener(object : TextWatcher {
             val tv_passwordDescription = findViewById<TextView>(R.id.tv_DescriptionPassword)
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
+            //텍스트 변화 있을 때 마다 실행
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
-                    if (editText.text.length == 0 || editText.text.isNullOrEmpty() || isEmpty(editText)
-                    ) {
-                        when (editText.id) {
-                            R.id.et_Id ->editText.error = "아이디를 입력하세요"
-                            R.id.et_Email -> editText.error ="이메일을 입력하세요"
-                            R.id.et_Password ->{
-                                editText.error = "비밀번호를 입력하세요"
-                                tv_passwordDescription.isVisible = true
-                            }
-                            R.id.et_ConfirmPassword -> editText.error ="비밀번호를 확인해주세요."
-                            else -> ""
+                if (editText.text.length == 0 || editText.text.isNullOrEmpty() || isEmpty(editText)
+                ) {
+                    when (editText.id) {
+                        R.id.et_Id -> editText.error = "아이디를 입력하세요"
+                        R.id.et_Email -> editText.error = "이메일을 입력하세요"
+                        R.id.et_Password -> {
+                            editText.error = "비밀번호를 입력하세요"
+                            tv_passwordDescription.isVisible = true
                         }
-                    }else{
-                        if(editText.id == R.id.et_Password){
-                            tv_passwordDescription.isVisible = false
+
+                        R.id.et_ConfirmPassword -> editText.error = "비밀번호를 확인해주세요."
+                        else -> ""
+                    }
+                } else {
+                    if (editText.id == R.id.et_Password) {
+                        tv_passwordDescription.isVisible = false
+                        if (check(editText) && editText.text.length >= 10) { //비밀번호가 유효하고 10자 이상인지
+
+                        } else {
+                            editText.error = "10자리 이상, 특수문자, 숫자포함"
                         }
-                        if(editText.id == R.id.et_ConfirmPassword){
-                            isEqualPassword(editText)
+
+                    }
+                    if (editText.id == R.id.et_ConfirmPassword) {
+                        if (isEqualPassword()) {                  //password와confirmPassword가 같은지 체크
+
+                        } else {
+                            editText.error = "비밀번호가 같지 않음"
                         }
                     }
-
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -118,33 +178,32 @@ class SignUpActivity : AppCompatActivity() {
         })
     }
 
-    private fun isEmpty(editText: EditText): Boolean {
+    private fun isEmpty(editText: EditText): Boolean {          //공백 문자 처리
         val empty = editText.text.toString()
         empty.replace(" ", "")
-        return if (empty.contains(" ")) true else false
+        return empty.contains(" ")
     }
 
-    private fun createSpinner(spinner: Spinner, emailForm: EditText) {
+    private fun createSpinner(spinner: Spinner, emailForm: EditText) {   //스피너 생성 및 리스너
         spinner.apply {
-            adapter = ArrayAdapter.createFromResource(
+            adapter = ArrayAdapter.createFromResource(//스피너 생성
                 baseContext,
                 R.array.email,
                 android.R.layout.simple_list_item_1
             )
-            onItemSelectedListener = object : OnItemSelectedListener {
+            onItemSelectedListener = object : OnItemSelectedListener { //리스너
                 override fun onItemSelected(
                     parent: AdapterView<*>?,
                     view: View?,
                     position: Int,
                     id: Long
                 ) {
-                    if (spinner.selectedItemPosition == 3) {
+                    if (spinner.selectedItemPosition == 3) {    //드롭메뉴 직접입력 선택시 직접입력하게 비움
                         emailForm.apply {
                             setText("")
                             setHint("직접입력")
                         }
-                    } else emailForm.setText(spinner.selectedItem.toString())
-
+                    } else emailForm.setText(spinner.selectedItem.toString()) // 드롭메뉴 아이템선택시 EditText 입력
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -153,19 +212,17 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
-    private fun isRegularPwd(password: EditText): Boolean {
-        return if (password.text.toString().contains("!@#$%^&*()-_=+")) true else false
+    private fun check(editText: EditText): Boolean { //특수문자 숫자 정규식
+        val pwd = """^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^+\-=])(?=\S+$).*$"""
+        val pattern = Pattern.compile(pwd)
+        return pattern.matcher(editText.text).matches()
     }
 
-    private fun isEqualPassword(editText: EditText):Boolean{
+
+    private fun isEqualPassword(): Boolean {   //password 와 confirmPassword가 같은지 체크
         val password = findViewById<EditText>(R.id.et_Password)
         val confirmPassword = findViewById<EditText>(R.id.et_ConfirmPassword)
-        return if(password.text.toString() == confirmPassword.text.toString()){
-            true
-        } else {
-            editText.error = "비밀번호가 같지 않습니다."
-            false
-        }
+        return password.text.toString() == confirmPassword.text.toString()
     }
 }
 
---
+++ app/src/main/java/com/example/introduce/const.kt
@@ -0,0 +1,8 @@
+package com.example.introduce
+
+const val INFO = "Infomation"
+const val ID = "id"
+const val EMAIL = "EMail"
+const val PASSWORD = "Password"
+const val CONFIRM_PASSWORD = "ConfirmPassword"
+const val EMAIL_FORM = "Email_form"
